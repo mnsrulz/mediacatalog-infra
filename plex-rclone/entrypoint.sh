791cb9f806bfdc11f82bd77f13429e2463b1c705
mnsrulz <manishrawat4u@gmail.com>
2026-01-12 18:06:11 -0500

    add plex rclone image

---
+++ plex-rclone/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start Rclone mount in the background
+rclone mount -v --allow-non-empty --allow-other --read-only --vfs-read-chunk-size=1M\ 
+        --vfs-read-chunk-size-limit=8M --dir-cache-time=5s media: /media &
+
+# Wait for Rclone to mount
+while ! mountpoint -q /media; do
+  echo "Waiting for Rclone mount..."
+  sleep 1
+done
+
+echo "Rclone mount is ready."
+
+# Start Plex
+exec /init
