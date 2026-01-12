6ed7ea5099d475b9066a16eb59daee6d8631470a
mnsrulz <manishrawat4u@gmail.com>
2026-01-12 18:29:06 -0500

    add media dir on boot

--- plex-rclone/entrypoint.sh
+++ plex-rclone/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /media
+
 # Start Rclone mount in the background
 rclone mount -v --allow-non-empty --allow-other --read-only --vfs-read-chunk-size=1M \
     --vfs-read-chunk-size-limit=8M --dir-cache-time=5s media: /media --config /config/rclone/rclone.conf &
