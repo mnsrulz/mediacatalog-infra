2dc68627ffe41cbbcf02f6c783c2d350658b3936
mnsrulz <manishrawat4u@gmail.com>
2026-01-12 22:18:13 -0500

    using different location for rclone config

--- plex-rclone/entrypoint.sh
+++ plex-rclone/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir -p /media
+mkdir -p /media           ## for storing the media content
+mkdir -p /rcloneconfig    ## for storing the rclone config
 
 # Start Rclone mount in the background
 rclone mount -v --allow-non-empty --allow-other --read-only --vfs-read-chunk-size=1M \
-    --vfs-read-chunk-size-limit=8M --dir-cache-time=5s media: /media --config /config/rclone/rclone.conf &
+    --vfs-read-chunk-size-limit=8M --dir-cache-time=5s media: /media --config /rcloneconfig/rclone.conf &
 
 # Wait for Rclone to mount
 while ! mountpoint -q /media; do
