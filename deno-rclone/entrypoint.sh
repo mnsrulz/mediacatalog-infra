ad1606d236efa98fc82c475e331c841bc9db0a4e
mnsrulz <manishrawat4u@gmail.com>
2026-01-18 13:20:40 -0500

    Enhance rclone mount with --dump-headers option
    Added --dump-headers option to rclone mount command.

--- deno-rclone/entrypoint.sh
+++ deno-rclone/entrypoint.sh
@@ -6,7 +6,7 @@ mkdir -p /rcloneconfig    ## for storing the rclone config
 
 # Start Rclone mount in the background
 rclone mount -v --allow-non-empty --allow-other --read-only --vfs-read-chunk-size=4M \
-    --vfs-read-chunk-size-limit=16M --vfs-cache-mode=full --buffer-size=256K --no-checksum \
+    --vfs-read-chunk-size-limit=16M --vfs-cache-mode=full --buffer-size=256K --no-checksum --dump-headers \
     --cache-dir=/cache --vfs-cache-max-size=1G blobmzodata: /data --config /rcloneconfig/rclone.conf &
 
 # Wait for Rclone to mount
